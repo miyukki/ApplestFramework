caab0e9296214ba731ffb76fd60275f4f7b61159
miyukki <yusei1128@gmail.com>
2014-01-07 00:42:36 +0900

    #7 テスト用Controllerの修正

--- test/Test.php
+++ test/Test.php
@@ -3,8 +3,22 @@ require(dirname(__FILE__).'/../Core.php');
 
 class TestController extends Controller {
 	public function exec() {
-		$hello = 'world';
-		if($hello !== 'world') {
+
+		// Model
+		$user1 = UserModel::create();
+		$user1->name = 'Jane';
+		$user1_id = $user1->save();
+
+		$user2 = UserModel::create();
+		$user2->name = 'Mike';
+		$user2_id = $user2->save();
+
+		if(UserModel::find_by_id($user1_id)->name !== 'Jane') {
+			echo 'Endpoint 1';
+			exit(1);
+		}
+		if(UserModel::find_by_id($user2_id)->name !== 'Mike') {
+			echo 'Endpoint 2';
 			exit(1);
 		}
 	}
